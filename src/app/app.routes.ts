c55193919b9ba41830f8aa588243902dc01067e5
tsteelematc <tsteele@madisoncollege.edu>
2025-11-12 17:07:29 -0600

    Added route and moved `<router-outlet>`
    Edit address bar to see route ! ! !

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TsteeleFaves } from './tsteele-faves/tsteele-faves';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "tsteele", component: TsteeleFaves}
+];
