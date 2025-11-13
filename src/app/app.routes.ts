7c322e599c9ebd647a9a54627fd8fc46242747ff
Kimberg23 <imbergk23@gmail.com>
2025-11-13 13:33:33 -0600

    Added my route...
    Tested by editing the URL addr bar in browser...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { KimbergFaves } from './kimberg-faves/kimberg-faves';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: "kimberg"
+        , component: KimbergFaves 
+    }
+];
