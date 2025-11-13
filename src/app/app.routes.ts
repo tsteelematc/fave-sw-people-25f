e89283f548c7de9b2bf791c37215bbc2968f0be2
droc101 <37421449+droc101@users.noreply.github.com>
2025-11-13 13:33:11 -0600

    Add route 'csmith' for 'CsmithFaves' component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {CsmithFaves} from './csmith-faves/csmith-faves';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'csmith',
+    component: CsmithFaves
+  }
+];
