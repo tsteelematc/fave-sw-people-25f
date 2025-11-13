45f59ee6d85ab2335d2212c96e02dc7595ddeb2c
Austin-Spriggs <aspriggs@madisoncollege.edu>
2025-11-13 13:30:32 -0600

    added my route to my component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AspriggsFavs } from './aspriggs-favs/aspriggs-favs';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: "aspriggs", component: AspriggsFavs }
+];
