f17260afaa7e3686e2c1ec531bddfececcfda3dc
yyang22-JS Frameworks <yyang22@madisoncollege.edu>
2025-11-13 13:25:09 -0600

    Generated my component...
    ng generate component yyang22-faves --skip-tests

---
+++ src/app/yyang22-faves/yyang22-faves.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-yyang22-faves',
+  imports: [],
+  templateUrl: './yyang22-faves.html',
+  styleUrl: './yyang22-faves.css',
+})
+export class Yyang22Faves {
+
+}
