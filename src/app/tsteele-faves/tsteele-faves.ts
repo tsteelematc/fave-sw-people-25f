77b6cffe9f998aac8a1316e93ead72028491e3b1
tsteelematc <tsteele@madisoncollege.edu>
2025-11-13 13:25:10 -0600

    Generated my component...
    ng generate component tsteele-faves --skip-tests

---
+++ src/app/tsteele-faves/tsteele-faves.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tsteele-faves',
+  imports: [],
+  templateUrl: './tsteele-faves.html',
+  styleUrl: './tsteele-faves.css',
+})
+export class TsteeleFaves {
+
+}
