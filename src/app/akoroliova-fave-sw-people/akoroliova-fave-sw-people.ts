143cbb0a25df2231ae0c1775762c2e427d59d4c1
matgeroina <157189615+matgeroina@users.noreply.github.com>
2025-11-13 13:25:13 -0600

    generated a new component

---
+++ src/app/akoroliova-fave-sw-people/akoroliova-fave-sw-people.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-akoroliova-fave-sw-people',
+  imports: [],
+  templateUrl: './akoroliova-fave-sw-people.html',
+  styleUrl: './akoroliova-fave-sw-people.css',
+})
+export class AkoroliovaFaveSwPeople {
+
+}
