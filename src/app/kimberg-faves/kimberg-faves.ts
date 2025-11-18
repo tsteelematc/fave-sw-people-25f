5f64abefb18aca3cd8a251953fb1bac1c7e78ca3
Kimberg23 <imbergk23@gmail.com>
2025-11-18 13:43:54 -0600

    updated code

--- src/app/kimberg-faves/kimberg-faves.ts
+++ src/app/kimberg-faves/kimberg-faves.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SwPeopleService } from '../sw-people.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-kimberg-faves',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './kimberg-faves.html',
   styleUrl: './kimberg-faves.css',
 })
 export class KimbergFaves {
+  private readonly peopleSvc = inject(SwPeopleService);
 
+  protected readonly people$ = this.peopleSvc.getPeopleFromSwapiApi();
 }
