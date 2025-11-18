d2d9518b5d4f4a79b247692d15661ca76859f813
Austin-Spriggs <aspriggs@madisoncollege.edu>
2025-11-18 13:37:41 -0600

    added service and updated html to be a dynamic list of swapi people

--- src/app/aspriggs-favs/aspriggs-favs.ts
+++ src/app/aspriggs-favs/aspriggs-favs.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SwPeopleService } from '../sw-people-service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-aspriggs-favs',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './aspriggs-favs.html',
   styleUrl: './aspriggs-favs.css',
 })
 export class AspriggsFavs {
+  private readonly peopleSvc = inject(SwPeopleService);
 
+  protected readonly people$ = this.peopleSvc.getPeopleFromSwapiApi();
 }
