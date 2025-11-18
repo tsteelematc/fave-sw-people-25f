683ee162cb13d5becf60bba8774a10940a05541c
tsteelematc <tsteele@madisoncollege.edu>
2025-11-18 13:07:42 -0600

    Setup our service to use built in Ng Http svc...
    https://angular.dev/guide/http/setup

--- src/app/sw-people.service.ts
+++ src/app/sw-people.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SwPeopleService {
-  
+  private http = inject(HttpClient);
 }
