f1672491be3c4ea8509c11ddfe98ad3e23771998
tsteelematc <tsteele@madisoncollege.edu>
2025-11-18 13:48:13 -0600

    Sort the people...

--- src/app/sw-people.service.ts
+++ src/app/sw-people.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map } from 'rxjs';
+import { map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +13,14 @@ export class SwPeopleService {
     return this.http.get<any>('https://swapi.dev/api/people').pipe(
       map(
         response => response.results
+      ),
+      tap(
+        x => console.log(x)
+      ),
+      map(
+        people => people.sort(
+          (a: any, b: any) => a.name.localeCompare(b.name)
+        )
       ),
     );
     
