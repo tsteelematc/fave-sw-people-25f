6d84fa7025458946f5a5ff8fc67fbdf4b08c3e8e
Austin-Spriggs <aspriggs@madisoncollege.edu>
2025-11-18 13:48:20 -0600

    sorts the people

--- src/app/sw-people-service.ts
+++ src/app/sw-people-service.ts
@@ -11,6 +11,9 @@ export class SwPeopleService {
   public getPeopleFromSwapiApi() {
     return this.http.get<any>('https://swapi.dev/api/people').pipe(
       map(response => response.results),
+      map(people => people.sort(
+        (a: any, b: any) => a.name.localeCompare(b.name)
+      ))
     );
   }
 }
