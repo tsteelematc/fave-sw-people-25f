752d569041a9ef04f949db2a965f7432a88da8c3
tsteelematc <tsteele@madisoncollege.edu>
2025-11-19 14:58:33 -0600

    Some service methods for Thu Week 12

--- src/app/sw-people.service.ts
+++ src/app/sw-people.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { EMPTY, expand, map, reduce, tap } from 'rxjs';
+import { EMPTY, expand, firstValueFrom, map, reduce, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -36,5 +36,31 @@ export class SwPeopleService {
       ),
     );
     
+  }
+
+  public getPeoplePageOne() {
+
+    // return Promise.reject("No soup for you : - )");
+
+    const pageObvservable = this.http.get<any>('https://swapi.dev/api/people/?page=1').pipe(
+      map(
+        response => response.results
+      ),
+    );
+
+    return firstValueFrom(pageObvservable);
+  }
+
+  public getPeoplePageTwo() {
+
+    // return Promise.reject("No candy for you : - )");
+
+    const pageObvservable = this.http.get<any>('https://swapi.dev/api/people/?page=2').pipe(
+      map(
+        response => response.results
+      ),
+    );
+
+    return firstValueFrom(pageObvservable);
   }
 }
