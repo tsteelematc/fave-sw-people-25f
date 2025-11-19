dc3213fdff5aefbe312f986fb2c01a365416beb0
tsteelematc <tsteele@madisoncollege.edu>
2025-11-19 14:59:47 -0600

    This was fun : - )

--- src/app/tsteele-faves/tsteele-faves.ts
+++ src/app/tsteele-faves/tsteele-faves.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SwPeopleService } from '../sw-people.service';
 import { AsyncPipe } from '@angular/common';
 
@@ -12,4 +12,58 @@ export class TsteeleFaves {
   private readonly peopleSvc = inject(SwPeopleService);
 
   protected readonly people$ = this.peopleSvc.getPeopleFromSwapiApi();
+
+  protected promisesAsThenables() {
+    
+    const page1 = this.peopleSvc.getPeoplePageOne()
+      .then(
+        data => {
+          console.log(data);
+
+          const page2 = this.peopleSvc.getPeoplePageTwo()
+            .then(
+              data => console.log(data)
+            )
+            .catch(
+              err => console.warn(err)
+            )
+        }
+      )
+      .catch(
+        err => console.warn(err)
+      )
+    ;
+  }
+
+  protected async promisesWithAsyncAwait() {
+    try {
+      const page1 = await this.peopleSvc.getPeoplePageOne();
+      console.log(page1) // ? ? ?
+
+      const page2 = await this.peopleSvc.getPeoplePageTwo();
+      console.log(page2) // ? ? ?
+    }
+    
+    catch(err) {
+      console.warn(err)
+    }
+  }
+
+  protected async promisesFun() {
+    try {
+      const page1 = this.peopleSvc.getPeoplePageOne();
+      console.log(page1) // ? ? ?
+
+      const page2 = this.peopleSvc.getPeoplePageTwo();
+      console.log(page2) // ? ? ?
+
+      // const data = await Promise.all([page1, page2]);
+      const data = await Promise.any([page1, page2]);
+      console.log(data[0]);
+    }
+    
+    catch(err) {
+      console.warn(err)
+    }
+  }
 }
