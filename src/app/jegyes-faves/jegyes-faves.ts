ccfc25d4e2abd790b2328ea9103c8fedcf210360
jegyes <jegyes@madisoncollege.edu>
2025-11-20 17:31:55 -0600

    generated my component
    ng generate component jegyes-faves --skip-tests

---
+++ src/app/jegyes-faves/jegyes-faves.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jegyes-faves',
+  imports: [],
+  templateUrl: './jegyes-faves.html',
+  styleUrl: './jegyes-faves.css',
+})
+export class JegyesFaves {
+
+}
