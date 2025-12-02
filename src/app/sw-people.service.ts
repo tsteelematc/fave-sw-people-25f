093b69f6a72e98a3f641cd365163920cea100625
bryson-fun <funmaker.bryson@gmail.com>
2025-12-02 10:33:33 -0600

    clearing to follow along with video
    cleared all sw-people service code to reconstruct and follow video.

--- src/app/sw-people.service.ts
+++ src/app/sw-people.service.ts
@@ -1,40 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+
 import { inject, Injectable } from '@angular/core';
-import { EMPTY, expand, map, reduce, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SwPeopleService {
-  private http = inject(HttpClient);
 
-  public getPeopleFromSwapiApi() {
-
-    return this.http.get<any>('https://swapi.dev/api/people').pipe(
-      expand(
-        page => page.next
-          ? this.http.get<any>(page.next)
-          : EMPTY
-      ),
-      map(
-        response => response.results
-      ),
-      tap(
-        x => console.log(x)
-      ),
-      reduce(
-        (acc: any[], people) => [
-          ...acc,
-          ...people,
-        ],
-        [],
-      ),
-      map(
-        people => people.sort(
-          (a: any, b: any) => a.name.localeCompare(b.name)
-        )
-      ),
-    );
-    
-  }
 }
