5f65ee3cae8f08546f759c9c707088ea4e78fc5b
bryson-fun <funmaker.bryson@gmail.com>
2025-12-02 10:38:22 -0600

    Setup our service to use built in ng http svc
    https://angular.dev/guide/http/setup

--- src/app/sw-people.service.ts
+++ src/app/sw-people.service.ts
@@ -1,9 +1,11 @@
 
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SwPeopleService {
-
+  private http = inject(HttpClient);
+  
 }
